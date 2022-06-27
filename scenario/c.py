21782dd0f4f388cf3413b95e20f669952f68a66d
Ahmad Reza Cheraghi <a_r_cheraghi@yahoo.com>
2022-06-27 11:37:55 +0200

    increases the swarm size to coat the cave completely

--- scenario/c.py
+++ scenario/c.py
@@ -1,7 +1,7 @@
 from scenario.particleGroups import *
 
 def scenario(world):
-	hexagon(world, (-45.0, 0.0, 0.0), 50)
+	hexagon(world, (-45.0, 0.0, 0.0), 49)
 	world.add_tile((7.5, 1.0, 0), color=(0.3, 0.3, 0.8, 1.0))
 	world.add_tile((7.0, 0.0, 0), color=(0.3, 0.3, 0.8, 1.0))
 	world.add_tile((8.0, 2.0, 0), color=(0.3, 0.3, 0.8, 1.0))
